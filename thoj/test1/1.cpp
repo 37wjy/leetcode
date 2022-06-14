9f6a40c1c7a1be72cfce4cc06bfb3af193549e01
wjy <37wjy@sina.com>
2022-06-14 11:49:27 +0800

    update

--- thoj/test1/1.cpp
+++ thoj/test1/1.cpp
@@ -2,136 +2,112 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+
 using namespace std;
+typedef long long ll;
+const int maxn = 100002;
+int n, m, k;
 
-// ================= 代码实现开始 =================
-
-int black=0;
 struct Edge
 {
-    int x, y, w, t; // 边的两个端点为x和y，边权为w。若为关键边则t=1，否则t=0
+    int u, v, w, t;
 };
-vector<int> root;
 
-int findRoot(int x)
+// edge[maxn];
+
+vector<int> fa(50001);
+vector<Edge> edge(maxn);
+
+void init()
 {
-    return x == root[x] ? x : root[x] = findRoot(root[x]);
+    for (int i = 0; i <= n; i++)
+        fa[i] = i;
 }
 
-bool concat(int x, int y)
+int find(int x)
 {
-    int fx = findRoot(x), fy = findRoot(y);
-    if (fx == fy)
+    return x == fa[x] ? fa[x] : fa[x] = find(fa[x]);
+}
+
+bool connect(int x, int y)
+{
+    x = find(x), y = find(y);
+    if (x == y)
         return false;
-    root[fx] = fy;
+    fa[x] = y;
     return true;
 }
 
-int fool(){
-
+void shift(ll x)
+{
+    for (int i = 1; i <= m; i++)
+        if (edge[i].t)
+            edge[i].w += x;
 }
 
-// n, m, k如题所述。edge为一个长度为m的Edge数组，每个元素描述一条边
-// 返回满足条件的最小生成树的权值之和
-int getAnswer(int n, int m, int k, vector<Edge> edge)
+ll cost = 0;
+ll kruskal(ll x)
 {
-    int ans = 0, kc = 0, i = 0, edge_cn = 0;
-    root.resize(n + 1);
+    init();
+    ll high = 0, sum = 0;
+    cost = 0;
+    shift(x);
+    sort(edge.begin() + 1, edge.begin() + m + 1, [](Edge l, Edge r){ return l.w == r.w ? l.t > r.t : l.w < r.w; });
 
-    for (; i <= n; i++)
-        root[i] = i;
-
-    sort(edge.begin(), edge.end(), [](Edge l, Edge r)
-         { return l.t == r.t ? l.w < r.w : l.t > r.t; });
-
-    for (int i = 0; i < black-k; i++)
+    for (int i = 1; i <= m and sum < n; i++)
     {
-        
-    }
-    
-
-    for (i = 0; i < m and kc < (n-k-1); i++)
-    {
-        int x = edge[i].x, y = edge[i].y;
-        if (concat(x, y))
+        if (connect(edge[i].u, edge[i].v))
         {
-            ans += edge[i].w;
-            edge_cn++;
-            kc++;
-            cout<<edge[i].x<<' '<<edge[i].y<<' '<<edge[i].w<<' '<<edge[i].t<<'\n';
+            sum++;
+            cost += edge[i].w;
+            if (edge[i].t)
+                high++;
         }
     }
+    shift(-x);
+    return high;
+}
 
-    
-    for (; i < m and edge_cn < n; i++)
-    {           
-        int x = edge[i].x, y = edge[i].y;
-        if (concat(x, y))
+ll solve(ll x)
+{
+    ll l = -x, r = x, mid;
+    ll ans = 0;
+    ll ck = 0;
+    while (l < r)
+    {
+        mid = (l + r) >> 1;
+        ck = kruskal(mid);
+        if (ck >= k)
         {
-            ans += edge[i].w;
-            edge_cn++;
-            cout<<edge[i].x<<' '<<edge[i].y<<' '<<edge[i].w<<'\n';
+            l = mid + 1;
+            ans = cost - k * mid;
+        }
+        else
+        {
+            r = mid;
         }
     }
-    //cout<<endl;
     return ans;
 }
 
-
-// ================= 代码实现结束 =================
-
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-
-    int n, m, k;
-    vector<Edge> edge;
+    int bl = INT32_MAX, br = 0, wl = INT32_MAX, wr = 0;
     cin >> n >> m >> k;
-
-    for (int i = 0; i < m; ++i)
+    for (int i = 1; i <= m; ++i)
     {
-        int x, y, w, t;
-        cin >> x >> y >> w >> t;
-        if (t)
+        cin >> edge[i].u >> edge[i].v >> edge[i].w >> edge[i].t;
+        if (edge[i].t)
         {
-            black++;
+            bl = min(bl, edge[i].w);
+            br = max(br, edge[i].w);
         }
-        edge.push_back((Edge){x, y, w, t});
+        else
+        {
+            wl = min(wl, edge[i].w);
+            wr = max(wr, edge[i].w);
+        }
     }
-
-    cout << getAnswer(n, m, k, std::move(edge)) << '\n';
+    cout << solve(ll(max(abs(bl - wr), abs(br - wl))) + 1) << '\n';
     return 0;
 }
-
-//对于 100% 的数据，n≤50000,m≤100000
-/*
-8 20 4
-5 3 22 1
-3 7 32 0
-5 2 24 0
-5 1 39 0
-3 6 31 0
-8 6 2 1
-1 7 25 1
-7 6 20 1
-3 6 96 1
-3 4 36 0
-2 3 32 1
-3 8 36 0
-7 1 77 0
-8 7 30 0
-5 4 81 0
-7 4 14 0
-8 6 21 0
-5 2 92 1
-1 8 66 0
-5 8 15 1
- */
-/* 
-8 6 2 1
-5 8 15 1
-7 6 20 1
-5 3 22 1
-
-*/
