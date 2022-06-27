641a30c637b846fb1b21e6659bd103aee83c4e9e
wjy <37wjy@sina.com>
2022-06-27 16:31:24 +0800

    update

--- thoj/test3/73.cpp
+++ thoj/test3/73.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
+//#include <vector>
+//#include <iostream>
 
 #define MAXN 1000000
+#define ll long long
 
 using namespace std;
 
@@ -10,13 +13,37 @@ int n;
 // 在此实现你的算法
 long long get_ans() {
 
+    vector<int> dp_leftSequence(n+1,0), dp_rightSequence(n+1,0);
+    dp_leftSequence[1] = a[1];
+    dp_rightSequence[n] = a[n]; //赋边界条件（初始状态）
+ 
+    for(int i = 1; i <= n; i ++)
+        dp_leftSequence[i] = max(a[i], dp_leftSequence[i - 1] + a[i]);
+        //状态转移方程
+    for(int i = 2; i <= n; i ++) //important optmization，简化寻找操作
+        if(dp_leftSequence[i] < dp_leftSequence[i - 1])
+            dp_leftSequence[i] = dp_leftSequence[i - 1];
+    
+    for(int i = n - 1; i >= 1; i --)
+        dp_rightSequence[i] = max(a[i], dp_rightSequence[i + 1] + a[i]);
+        //状态转移方程
+    for(int i = n - 1; i >= 1; i --) //important optmization，简化寻找操作
+        if(dp_rightSequence[i] < dp_rightSequence[i + 1])
+            dp_rightSequence[i] = dp_rightSequence[i + 1];
+ 
+    ll tmp = 0, maxn = -1;
+ 
+    for(int i = 1; i <= n-1; i ++) { //遍历每个点
+        tmp = dp_leftSequence[i] + dp_rightSequence[i + 1];
+        maxn = max(tmp, maxn);
+    }
+    return maxn;
 }
 
 int main() {
     scanf("%d", &n);
     for (int i = 1; i <= n; i++) {
-        int a;
-        scanf("%d", &a);
+        scanf("%d", &a[i]);
     }
 
     printf("%lld\n", get_ans());
--- thoj/test3/83.cpp
+++ thoj/test3/83.cpp
@@ -8,13 +8,53 @@
 using namespace std;
 
 char a[MAXN + 10], b[MAXN + 10];
+int dp[MAXN + 10][MAXN + 10];
 int na, nb;
 
-int solve() {
-    return 233;
+int s[6][6] = {
+    {0, 1, 1, 1, 1, 0},
+    {1, 4, 2, 2, 2, 1},
+    {1, 2, 4, 2, 2, 1},
+    {1, 2, 2, 4, 2, 1},
+    {1, 2, 2, 2, 4, 1},
+    {0, 1, 1, 1, 1, 0}};
+int change(char a)
+{
+    if (a == 'A')
+        return 1;
+    if (a == 'G')
+        return 3;
+    if (a == 'C')
+        return 2;
+    if (a == 'T')
+        return 4;
+    else
+        return 0;
+}
+int solve()
+{
+    int i=0;
+    for (int i = 1; i <= na; i++)
+        a[i] = change(a[i]);
+    for (int i = 1; i <= nb; i++)
+        b[i] = change(b[i]);
+
+    for (int i = 1; i <= na; i++)
+        dp[0][i] = dp[0][i - 1] + s[5][a[i]];
+
+    for (int i = 1; i <= nb; i++)
+        dp[i][0] = dp[i - 1][0] + s[b[i]][5]; // cout<<"a";
+    
+    for (int i = 1; i <= nb; i++)
+        for (int j = 1; j <= na; j++)
+            dp[i][j] = max(
+                max(dp[i - 1][j - 1] + s[b[i]][a[j]], dp[i - 1][j] + s[0][b[i]]),
+                dp[i][j - 1] + s[0][a[j]]);
+    return dp[nb][na];
 }
 
-int main() {
+int main()
+{
     scanf("%d%s", &na, a + 1);
     scanf("%d%s", &nb, b + 1);
     printf("%d\n", solve());
